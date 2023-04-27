a65b7ab7372e55cce14bbbdf345bbeb07103eaff
Mannam <S563096@nwmissouri.edu>
2023-04-27 02:24:10 -0500

    bonus

--- models/perfume.js
+++ models/perfume.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 const perfumechema = mongoose.Schema({
-brand: String,
-fragrance: String,
-cost: Number
+brand: {type:String,minLength:2,maxLength:67},
+fragrance: {type:String,minLength:3,maxLength:80},
+cost: {type:Number,min:5,maxLength:1200},
 })
 module.exports = mongoose.model("perfume",
 perfumechema)
--- routes/perfume.js
+++ routes/perfume.js
@@ -3,13 +3,20 @@ const perfume_controlers= require('../controllers/perfume');
 var router = express.Router();
 /* GET perfume */
 router.get('/', perfume_controlers.perfume_view_all_Page );
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 /*GET detail perfume page */
 router.get('/detail', perfume_controlers.perfume_view_one_Page);
 module.exports = router;
 /* GET create perfume page */
-router.get('/create', perfume_controlers.perfume_create_Page);
+router.get('/create',secured, perfume_controlers.perfume_create_Page);
 /* GET create update page */
 router.get('/update', perfume_controlers.perfume_update_Page); 
 /* GET delete perfume page */
-router.get('/delete', perfume_controlers.perfume_delete_Page);
+router.get('/delete',secured, perfume_controlers.perfume_delete_Page);
 
